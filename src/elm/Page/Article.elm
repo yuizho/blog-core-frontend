73d34ed3c19d01a7b0bf7313cf050b71f2560d70
yuizho <yuizho3@gmail.com>
2019-01-17 00:29:23 +0900

    modify Article.elm
    add editor/preview tab in Article screen

--- src/elm/Page/Article.elm
+++ src/elm/Page/Article.elm
@@ -17,9 +17,15 @@ import Url.Builder as UrlBuilder
 type alias Model =
     { articleInfo : ArticleInfo
     , content : String
+    , editMode : EditMode
 
     -- TOOD: ここでLoadking状態とかもたせればよさげ。
     }
+
+
+type EditMode
+    = Editor
+    | Preview
 
 
 type alias ArticleInfo =
@@ -30,7 +36,7 @@ type alias ArticleInfo =
 
 init : String -> ( Model, Cmd Msg )
 init id =
-    ( Model (ArticleInfo "" "") ""
+    ( Model (ArticleInfo "" "") "" Editor
     , fetchContent id
     )
 
@@ -42,6 +48,8 @@ init id =
 type Msg
     = ShowContent (Result Http.Error Model)
     | ChangeContent String
+    | ClickedEditor
+    | ClickedPreview
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -65,6 +73,16 @@ update msg model =
             , Cmd.none
             )
 
+        ClickedEditor ->
+            ( { model | editMode = Editor }
+            , Cmd.none
+            )
+
+        ClickedPreview ->
+            ( { model | editMode = Preview }
+            , Cmd.none
+            )
+
 
 
 -- VIEW
@@ -72,6 +90,23 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
+    let
+        editModeConfig =
+            case model.editMode of
+                Editor ->
+                    { editorDisplay = "block"
+                    , editorTabclass = [ class "siimple-tabs-item", class "siimple-tabs-item--selected" ]
+                    , previewDisplay = "none"
+                    , previewTabclass = [ class "siimple-tabs-item" ]
+                    }
+
+                Preview ->
+                    { editorDisplay = "none"
+                    , editorTabclass = [ class "siimple-tabs-item" ]
+                    , previewDisplay = "block"
+                    , previewTabclass = [ class "siimple-tabs-item", class "siimple-tabs-item--selected" ]
+                    }
+    in
     div []
         [ div
             [ class "siimple-jumbotron"
@@ -79,22 +114,25 @@ view model =
             [ div [ class "siimple-jumbotron-title" ] [ text model.articleInfo.title ]
             , div [ class "siimple-jumbotron-detail" ] [ text <| "Posted at " ++ model.articleInfo.createdAt ]
             ]
-        , div
-            [ class "siimple-rule"
-            , class "siimple--color-dark"
-            ]
-            []
         , div []
-            [ div []
+            [ div
+                [ class "siimple-tabs"
+                , class "siimple-tabs--boxed"
+                ]
+                [ div (onClick ClickedEditor :: editModeConfig.editorTabclass) [ text "Editor" ]
+                , div (onClick ClickedPreview :: editModeConfig.previewTabclass) [ text "Preview" ]
+                ]
+            , div []
                 [ textarea
                     [ class "siimple-textarea"
                     , class "siimple-textarea--fluid"
-                    , rows 10
+                    , rows 25
                     , onInput ChangeContent
+                    , style "display" editModeConfig.editorDisplay
                     ]
                     [ text model.content ]
                 ]
-            , div [] [ toHtmlWith options [] model.content ]
+            , div [ style "display" editModeConfig.previewDisplay ] [ toHtmlWith options [] model.content ]
             ]
         ]
 
@@ -120,7 +158,7 @@ fetchContent id =
     -- I refer this redit
     -- https://www.reddit.com/r/elm/comments/91t937/is_it_possible_to_make_multiple_http_requests_in/
     Task.attempt ShowContent <|
-        Task.map2 (\articleInfo content -> Model articleInfo content) articleTask contentTask
+        Task.map2 (\articleInfo content -> Model articleInfo content Editor) articleTask contentTask
 
 
 contentUrl : String -> String
