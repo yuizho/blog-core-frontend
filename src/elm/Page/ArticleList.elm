3dafe92181a0f0c161bbcfbd50e1d6e348ac62d8
yuizho <yuizho3@gmail.com>
2019-03-28 15:25:12 +0900

    modify ArticleList
    - modify designe of ArticleList

--- src/elm/Page/ArticleList.elm
+++ src/elm/Page/ArticleList.elm
@@ -83,15 +83,13 @@ view model =
                 Success articles ->
                     List.map viewArticle articles
     in
-    div [ class "siimple-grid-row" ] contents
+    div [ class "siimple-content--extra-small" ] contents
 
 
 viewArticle : Article -> Html msg
 viewArticle article =
     div
-        [ class "siimple-grid-col"
-        , class "siimple-grid-col--9"
-        ]
+        [ class "siimple--mb-4" ]
         [ a
             [ class "siimple-link"
             , class "siimple--color-dark"
